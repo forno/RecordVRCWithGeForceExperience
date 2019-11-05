3b583ef94b6e98da9babaa30b89f94dd44517e7e
FORNO <forno@xmaho.link>
2019-11-05 21:23:12 +0900

    Add argument for focus process

--- recordVRC.ps1
+++ recordVRC.ps1
@@ -1,4 +1,6 @@
-$process = Get-process -Name VRChat
+Param([String]$title = "VRChat")
+
+$process = Get-process -Name $title
 (New-Object -ComObject WScript.Shell).AppActivate($process.MainWindowTitle)
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait("%{F9}")
